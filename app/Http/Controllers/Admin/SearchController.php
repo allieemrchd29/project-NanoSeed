20d35093d18588763352fe3e884d0e87b1a0b7ae
Aliya Marcelia Dewi <aliyamarceliadewi@gmail.com>
2026-04-29 06:31:50 +0700

    mengupdate fitur admin search

--- app/Http/Controllers/Admin/SearchController.php
+++ app/Http/Controllers/Admin/SearchController.php
@@ -5,14 +5,26 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Kampanye;
+use App\Models\Dampak;       
+use App\Models\Dokumentasi;  
 
 class SearchController extends Controller
 {
     public function index(Request $request)
     {
         $query = $request->input('q');
-        $hasil = Kampanye::where('nama_kampanye', 'LIKE', "%{$query}%")->get();
-        
-        return view('admin.search-results', compact('hasil', 'query'));
+
+        $kampanye = Kampanye::where('nama_kampanye', 'LIKE', "%{$query}%")
+                        ->orWhere('deskripsi', 'LIKE', "%{$query}%")
+                        ->get();
+
+        $dokumentasi = Dokumentasi::where('keterangan', 'LIKE', "%{$query}%")
+                        ->orWhereHas('kampanye', function($q) use ($query) {
+                            $q->where('nama_kampanye', 'LIKE', "%{$query}%");
+                        })
+                        ->with('kampanye') // eager load relasi kampanye
+                        ->get();
+
+        return view('admin.search', compact('kampanye', 'dokumentasi', 'query'));
     }
 }
