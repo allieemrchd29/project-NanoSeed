80dd2c35d38880d3d65926e973f63a0989c8c7a0
Misar <misaradinda27@gmail.com>
2026-05-03 04:11:25 +0700

    menambahkan formulir donatur di file donasi

--- resources/views/components/navbar-admin.blade.php
+++ resources/views/components/navbar-admin.blade.php
@@ -78,6 +78,7 @@
         </button>
 
         <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
+            <span class="fs-2">🌱</span>
             <span class="fs-2">NanoSeed</span>
         </h1>
 
